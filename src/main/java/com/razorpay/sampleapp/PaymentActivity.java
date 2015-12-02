f900b426b80aeb9a8c34d113ffc39089fda1785f
Shashank Mehta <me@shashankmehta.in>
2015-12-02 14:24:28 +0530

    Cleans up sample code

--- src/main/java/com/razorpay/sampleapp/PaymentActivity.java
+++ src/main/java/com/razorpay/sampleapp/PaymentActivity.java
@@ -12,7 +12,6 @@ import org.json.JSONObject;
 
 public class PaymentActivity extends Activity
 {
-  final String public_key = "rzp_live_ILgsfZCZoFIKMb";
 
   public PaymentActivity(){}
 
@@ -25,32 +24,44 @@ public class PaymentActivity extends Activity
     // payment button created by you in xml layout
     View button = (View) findViewById(R.id.pay_btn);
 
-    // you need to pass current activity in order to let razorpay create CheckoutActivity
-    final Activity activity = this;
-    final CheckoutFragment co = new CheckoutFragment();
-    co.setPublicKey(public_key);
-
     button.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View arg0) {
-        try{
-          JSONObject options = new JSONObject("{" +
-            "description: 'Demoing Charges'," +
-            "image: 'https://rzp-mobile.s3.amazonaws.com/images/rzp.png'," +
-            "currency: 'INR'}"
-          );
-
-          options.put("amount", "100");
-          options.put("name", "Razorpay Corp");
-          options.put("prefill", new JSONObject("{email: 'sm@razorpay.com', contact: '9876543210'}"));
-
-          co.open(activity, options);
-
-        } catch(Exception e){
-          Toast.makeText(activity, e.getMessage(), Toast.LENGTH_SHORT).show();
-          e.printStackTrace();
-        }
+        startPayment();
       };
     });
+  }
+
+  public void startPayment(){
+    /**
+     * Replace with your public key
+     */
+    final String public_key = "rzp_live_ILgsfZCZoFIKMb";
+
+    /**
+     * You need to pass current activity in order to let razorpay create CheckoutActivity
+     */
+    final Activity activity = this;
+
+    final CheckoutFragment co = new CheckoutFragment();
+    co.setPublicKey(public_key);
+
+    try{
+      JSONObject options = new JSONObject("{" +
+        "description: 'Demoing Charges'," +
+        "image: 'https://rzp-mobile.s3.amazonaws.com/images/rzp.png'," +
+        "currency: 'INR'}"
+      );
+
+      options.put("amount", "100");
+      options.put("name", "Razorpay Corp");
+      options.put("prefill", new JSONObject("{email: 'sm@razorpay.com', contact: '9876543210'}"));
+
+      co.open(activity, options);
+
+    } catch(Exception e){
+      Toast.makeText(activity, e.getMessage(), Toast.LENGTH_SHORT).show();
+      e.printStackTrace();
+    }
   }
 }
