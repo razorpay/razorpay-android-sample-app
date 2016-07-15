70d9188efd16d818856227c534977dc15018b07a
Sachin Tewari <xachin@gmail.com>
2016-07-15 16:10:42 +0530

    [style] Refactor code according to Android standards
    (cherry picked from commit 8a6f9b1)

--- src/main/java/com/razorpay/sampleapp/PaymentActivity.java
+++ src/main/java/com/razorpay/sampleapp/PaymentActivity.java
@@ -1,96 +1,98 @@
 package com.razorpay.sampleapp;
 
 import android.app.Activity;
-import android.view.View;
 import android.os.Bundle;
-import android.widget.Toast;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.razorpay.Checkout;
+
 import org.json.JSONObject;
 
-public class PaymentActivity extends Activity
-{
+public class PaymentActivity extends Activity {
+    private static final String TAG = PaymentActivity.class.getSimpleName();
 
-  public PaymentActivity(){}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-  @Override
-  public void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
-    // payment button created by you in xml layout
-    View button = (View) findViewById(R.id.pay_btn);
+        // Payment button created by you in XML layout
+        Button button = (Button) findViewById(R.id.btn_pay);
 
-    button.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View arg0) {
-        startPayment();
-      };
-    });
-  }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPayment();
+            }
+        });
+    }
 
-  public void startPayment(){
+    public void startPayment() {
+        /**
+         * Replace with your public key
+         */
+        final String public_key = "rzp_live_ILgsfZCZoFIKMb";
+
+        /**
+         * You need to pass current activity in order to let Razorpay create CheckoutActivity
+         */
+        final Activity activity = this;
+
+        final Checkout co = new Checkout();
+        co.setPublicKey(public_key);
+
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Razorpay Corp");
+            options.put("description", "Demoing Charges");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://rzp-mobile.s3.amazonaws.com/images/rzp.png");
+            options.put("currency", "INR");
+            options.put("amount", "100");
+
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "sm@razorpay.com");
+            preFill.put("contact", "9876543210");
+
+            options.put("prefill", preFill);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
+    }
+
     /**
-     * Replace with your public key
+     * The name of the function has to be
+     * onPaymentSuccess
+     * Wrap your code in try catch, as shown, to ensure that this method runs correctly
      */
-    final String public_key = "rzp_live_ILgsfZCZoFIKMb";
+    @SuppressWarnings("unused")
+    public void onPaymentSuccess(String razorpayPaymentID) {
+        try {
+            Toast.makeText(this, "Payment Successful: " + razorpayPaymentID, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in onPaymentSuccess", e);
+        }
+    }
 
     /**
-     * You need to pass current activity in order to let razorpay create CheckoutActivity
+     * The name of the function has to be
+     * onPaymentError
+     * Wrap your code in try catch, as shown, to ensure that this method runs correctly
      */
-    final Activity activity = this;
-
-    final Checkout co = new Checkout();
-    co.setPublicKey(public_key);
-
-    try{
-      JSONObject options = new JSONObject("{" +
-        "description: 'Demoing Charges'," +
-        "image: 'https://rzp-mobile.s3.amazonaws.com/images/rzp.png'," +
-        "currency: 'INR'}"
-      );
-
-      options.put("amount", "100");
-      options.put("name", "Razorpay Corp");
-      options.put("prefill", new JSONObject("{email: 'sm@razorpay.com', contact: '9876543210'}"));
-
-      co.open(activity, options);
-
-    } catch(Exception e){
-      Toast.makeText(activity, e.getMessage(), Toast.LENGTH_SHORT).show();
-      e.printStackTrace();
+    @SuppressWarnings("unused")
+    public void onPaymentError(int code, String response) {
+        try {
+            Toast.makeText(this, "Payment failed: " + Integer.toString(code) + " " + response, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in onPaymentError", e);
+        }
     }
-  }
-
-  /**
-  * The name of the function has to be
-  *   onPaymentSuccess
-  * Wrap your code in try catch, as shown, to ensure that this method runs correctly
-  */
-  public void onPaymentSuccess(String razorpayPaymentID){
-    try {
-      Toast.makeText(this, "Payment Successful: " + razorpayPaymentID, Toast.LENGTH_SHORT).show();
-    }
-    catch (Exception e){
-      Log.e("com.merchant", e.getMessage(), e);
-    }
-  }
-
-  /**
-  * The name of the function has to be
-  *   onPaymentError
-  * Wrap your code in try catch, as shown, to ensure that this method runs correctly
-  */
-  public void onPaymentError(int code, String response){
-    try {
-      Toast.makeText(this, "Payment failed: " + Integer.toString(code) + " " + response, Toast.LENGTH_SHORT).show();
-    }
-    catch (Exception e){
-      Log.e("com.merchant", e.getMessage(), e);
-    }
-  }
 }
